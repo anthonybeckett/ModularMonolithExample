e20ee1a38bb2e4d72c1e4e6ae62f4bcdb791d8e4
AnthonyBeckett <antneyb10@gmail.com>
2026-04-01 22:22:34 +0100

    Added a caching service and using redis through docker

--- src/Evently.Modules.Events.Presentation/Categories/GetCategories.cs
+++ src/Evently.Modules.Events.Presentation/Categories/GetCategories.cs
@@ -1,3 +1,4 @@
+using Evently.Common.Application.Caching;
 using Evently.Common.Domain;
 using Evently.Modules.Events.Application.Categories.GetCategories;
 using Evently.Modules.Events.Application.Categories.GetCategory;
@@ -6,6 +7,7 @@ using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Evently.Modules.Events.Presentation.Categories;
 
@@ -13,9 +15,22 @@ internal static class GetCategories
 {
 	public static void MapEndpoint(IEndpointRouteBuilder app)
 	{
-		app.MapGet("categories", async (ISender sender) =>
+		app.MapGet("categories", async (ISender sender, ICacheService cacheService) =>
 			{
+				IReadOnlyCollection<CategoryResponse> cachedCategoryResponse = await cacheService.GetAsync<IReadOnlyCollection<CategoryResponse>>(
+					"categories");
+
+				if (cachedCategoryResponse is not null)
+				{
+					return Results.Ok(cachedCategoryResponse);
+				}
+
 				Result<IReadOnlyCollection<CategoryResponse>> result = await sender.Send(new GetCategoriesQuery());
+
+				if (result.IsSuccess)
+				{
+					await cacheService.SetAsync("categories", result.Value);
+				}
 
 				return result.Match(Results.Ok, ApiResults.ApiResults.Problem);
 			})
